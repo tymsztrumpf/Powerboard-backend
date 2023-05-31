44b09e707b58d1b3eb010ab30b904b934bf72142
tymsztrumpf <tymoteusztrumpf@gmail.com>
2023-05-31 14:37:44 +0200

    project initialzr

---
+++ src/main/java/powerboard/powerboard/PowerboardApplication.java
@@ -0,0 +1,13 @@
+package powerboard.powerboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerboardApplication.class, args);
+	}
+
+}
