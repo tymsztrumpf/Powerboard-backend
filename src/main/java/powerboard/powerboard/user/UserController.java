54ba9d4d391eb0f29d33267090f980aca9ab31f4
tymsztrumpf <tymoteusztrumpf@gmail.com>
2023-06-29 15:31:06 +0200

    implementation of getUser in User Controller

---
+++ src/main/java/powerboard/powerboard/user/UserController.java
@@ -0,0 +1,18 @@
+package powerboard.powerboard.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService userService;
+    @GetMapping
+    public ResponseEntity<UserDTO> getUser() {
+        return ResponseEntity.ok(userService.getCurrentUser());
+    }
+}
--- src/main/java/powerboard/powerboard/user/UserService.java
+++ src/main/java/powerboard/powerboard/user/UserService.java
@@ -1,11 +1,20 @@
 package powerboard.powerboard.user;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import powerboard.powerboard.exception.ApiRequestException;
 
 @Service
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserDTOMapper userDTOMapper;
+    public UserDTO getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
+        return userDTOMapper.apply(userRepository.findByEmail(user.getEmail()).orElseThrow(() -> new ApiRequestException("User not found")));
+    }
 
 }
