b6651bda39261035301ddf0a93d950400d3ed104
tymsztrumpf <tymoteusztrumpf@gmail.com>
2023-09-06 14:41:30 +0200

    code cleanup

--- src/main/java/powerboard/powerboard/config/WebSocketConfig.java
+++ src/main/java/powerboard/powerboard/config/WebSocketConfig.java
@@ -12,19 +12,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Set prefix for the endpoint that the client listens for our messages from
         registry.enableSimpleBroker("/topic");
 
-        // Set prefix for endpoints the client will send messages to
         registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp")
-                // Allow the origin http://localhost:3000 to send messages to us. (Base URL of the client)
                 .setAllowedOrigins("http://localhost:3000")
-                // Enable SockJS fallback options
                 .withSockJS();
     }
 }
